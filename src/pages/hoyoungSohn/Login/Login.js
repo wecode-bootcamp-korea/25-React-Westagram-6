a8111606968a9cf083c5e0488a133d5d58e683e7
youthangno <youthangno@gmail.com>
2021-09-26 21:30:56 +0900

    로긴창 react 구현중

--- src/pages/hoyoungSohn/Login/Login.js
+++ src/pages/hoyoungSohn/Login/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 import './Login.scss';
 
 class LoginHoYoung extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      titleColor: 'b9dffc',
+      value: 'a',
+    };
+  }
+
+  changeColor = () => {
+    this.setState({
+      titleColor: 'blue',
+    });
+  };
+
   render() {
+    let inpp = document.getElementsByTagName('input')[0];
+    console.log(document.getElementsByClassName('loginBtn'));
     return (
       <>
         <div className="container">
@@ -12,10 +28,15 @@ class LoginHoYoung extends React.Component {
               type="text"
               name="id"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              onInput={this.state.value}
             />
             <input type="password" name="password" placeholder="비밀번호" />
             {/* <Link to = './Main'><button className ='loginBtn'>로그인</button></Link> */}
-            <button className="loginBtn" onClick={this.gotoMain}>
+            <button
+              className="loginBtn"
+              style={{ backgroundColor: this.state.titleColor }}
+              onInput={this.changeColor}
+            >
               로그인
             </button>
           </form>
