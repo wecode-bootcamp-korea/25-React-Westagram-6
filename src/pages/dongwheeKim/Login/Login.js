6f9b62d512db66c9f5a6b10102796c5e3ca5ab45
Dongwhee Kim <don@MacBook-Pro-2.local>
2021-09-27 16:29:15 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/dongwheeKim/Login/Login.js
+++ src/pages/dongwheeKim/Login/Login.js
@@ -1,3 +1,4 @@
+import { faRubleSign } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Login.scss';
 
@@ -11,6 +12,7 @@ class LoginDongWhee extends React.Component {
     this.state = {
       textId: '',
       textPw: '',
+      loginButtonColor: false,
     };
   }
 
@@ -26,7 +28,40 @@ class LoginDongWhee extends React.Component {
     });
   };
 
+  toggleLoginButton = () => {
+    // if (this.state.loginButtonColor) {
+    //   this.setState({
+    //     loginButtonColor: false,
+    //   });
+    // } else {
+    //   this.setState({
+    //     loginButtonColor: true,
+    //   });
+    // }
+
+    //   this.setState({
+    //     loginButtonColor: this.state.loginButtonColor ? false : true,
+    //   });
+    // };
+
+    // this.setState({
+    //   loginButtonColor: !this.state.loginButtonColor,
+    // });
+
+    if (this.state.textId.includes('@') && this.state.textPw.length > 4) {
+      this.setState({
+        loginButtonColor: true,
+      });
+    } else {
+      this.setState({
+        loginButtonColor: false,
+      });
+    }
+  };
+
   render() {
+    console.log(this.state);
+    const { loginButtonColor } = this.state;
     return (
       <main className="LoginDongWhee">
         <div className="loginLayout">
@@ -40,6 +75,7 @@ class LoginDongWhee extends React.Component {
                 className="loginId"
                 placeholder="  전화번호, 사용자 이름 또는 이메일"
                 onChange={this.handleIdInput}
+                onKeyUp={this.toggleLoginButton}
               />
               <input
                 type="password"
@@ -48,13 +84,16 @@ class LoginDongWhee extends React.Component {
                 className="loginPw"
                 placeholder="  비밀번호"
                 onChange={this.handlePwInput}
+                onKeyUp={this.toggleLoginButton}
               />
             </div>
 
             <div className="loginBottom">
               <button
                 onClick={this.goToMain}
-                className="loginButton"
+                className={
+                  loginButtonColor ? 'loginButtonOn' : 'loginButtonOff'
+                }
                 type="button"
               >
                 로그인
